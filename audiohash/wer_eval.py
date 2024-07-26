d88d63a5465ccb50ea8420ed0ebf548baa12c31e
Raunak <ra3295@columbia.edu>
2024-07-26 11:26:13 -0400

    Change filenames

--- audiohash/wer_eval.py
+++ audiohash/wer_eval.py
@@ -1,49 +1,88 @@
-from datasets import load_dataset
+from faster_whisper import WhisperModel
 import evaluate
 import torch
-import scipy.io.wavfile as wav
 import os
-import codecs, json
+from whisper_normalizer.english import EnglishTextNormalizer
+import json
+
 
 def main():
-    #Data contains data['source'] to grab source of audio, if source==2 --> Youtube
-    youtube = 2
-
-    sample_rate = 16000
+    device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
+    print('Using:', device)
     json_dir = '/media/raunak/1TB/data_entries/'
     wav_dir = '/media/raunak/1TB/yt_wav_files/'
 
-    #Requires Hugging Face account & token due to being a gated dataset
-    #Grabbing xl training subset, aiming to download 3845 hours of YouTube data
-    gigaspeech_train = load_dataset("speechcolab/gigaspeech", "xl", split="train", streaming=True, token="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")
+    model = WhisperModel("base.en", device=device, compute_type="float16")
+    wer = evaluate.load("wer")
+    normalize = EnglishTextNormalizer()
+    
+    json_files = os.scandir(json_dir)
+    predictions = []
+    references = []
+    checked_data_dict = dict()
 
-    #Debugging Purposes
-    #gigaspeech_train = gigaspeech_train.take(10)
+    noise = 'noisy_data.txt'
+    check = 'checked_data.txt'
+    
+    if os.path.exists(noise):
+        os.remove(noise)
+    noisy_data = open(noise, 'a')
+    
+    if os.path.exists(check):
+        with open(check) as file:
+            for line in file:
+                line = line.strip('\n')
+                id, pred = line.split(':')
+                checked_data_dict[id] = pred
 
-    if not os.path.isdir(wav_dir):
-        os.makedirs(wav_dir)
-    if not os.path.isdir(json_dir):
-        os.makedirs(json_dir)
+    checked_data = open(check, 'a')
+    
+    gigaspeech_garbage_utterance_tags = ['<SIL>', '<NOISE>', '<MUSIC>', '<OTHER>']
+    
+    for (counter, file) in enumerate(json_files):
+        file_id = os.path.splitext(file.name)[0]
+        entry = open(f"{json_dir}{file.name}")
+        data = json.load(entry)
+        
+        if data['text'] in gigaspeech_garbage_utterance_tags:
+            noisy_data.write(f"{file_id}\n")
+            print(f"{counter}:Noise")
+            continue
+        
+        if file_id in checked_data_dict.keys():
+            predictions.append(checked_data_dict[file_id])
+            references.append(normalize(data['text']))
+            print(f"{counter}:Checked")
+            continue
+        
+        segments, _ = model.transcribe(f"{wav_dir}{file_id}.wav", beam_size=5)
+        prediction = ""
+        for segment in segments:
+            prediction += segment.text
+        
+        prediction = normalize(prediction)
+        print(f"{counter}")
+        checked_data.write(f"{file_id}:{prediction}\n")
+        references.append(normalize(data['text']))
+        predictions.append(prediction)
+        entry.close()
 
-    i = 0
-    for data in gigaspeech_train:
-        if data['source'] == youtube:
-            file = os.path.basename(data['audio']['path'])
-            json_file = f"{json_dir}{data['segment_id']}.json"
-            #Should add a check to see if data already in data_entries/
-            #And if wav file already in yt_wav_files/
-            if os.path.exists(f"{json_file}"):
-                print(f"{i}: Already Created")
-                continue
-            wav.write(f"{wav_dir}{file}", sample_rate, data['audio']['array'])
-            data['audio']['array'] = data['audio']['array'].tolist().clear()
-            data['audio']['path'] = f"{wav_dir}{file}"
-            json.dump(data, codecs.open(json_file, 'w', encoding='utf-8'), 
-            separators=(',', ':'), 
-            sort_keys=True, 
-            indent=4)
-            print(f"{i}: {data['segment_id']}.json created")
-            i = i + 1
+    noisy_data.close()
+    checked_data.close()
+
+    wer_metric = wer.compute(references=references, predictions=predictions)
+    wer_metric = round(100 * wer_metric, 2)
+    print('WER Rate: ',wer_metric, '%')
+
+
+    
+
+
+
+
+
+
+
 
 if __name__=='__main__':
     main()
