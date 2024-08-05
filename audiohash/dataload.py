7b205712faa12579de0dfd0b67396f61c4c92e0c
ragrawal123 <48493530+ragrawal123@users.noreply.github.com>
2024-08-05 11:00:32 -0700

    Update dataload.py

--- audiohash/dataload.py
+++ audiohash/dataload.py
@@ -14,7 +14,7 @@ def main():
     wav_dir = '/media/storage/yt_wav_files/'
 
     #Requires Hugging Face account & token due to being a gated dataset
-    #Grabbing xl training subset, aiming to download 3845 hours of YouTube data
+    #Grabbing xl training subset, aiming to download 1100 hours of YouTube data
     gigaspeech_train = load_dataset("speechcolab/gigaspeech", "xl", split="train", streaming=True, token="")
 
     #Debugging Purposes
